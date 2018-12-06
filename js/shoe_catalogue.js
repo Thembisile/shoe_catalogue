ddfcd7d8298647ce155bd30aacd91741909636a1
Thembisile <shaundamon6@icloud.com>
2018-12-06 22:01:57 +0200

    initial commit

---
+++ js/shoe_catalogue.js
@@ -0,0 +1,11 @@
+function shoe_catalogue(storedItems){
+
+
+    let itemsData = [
+        { color: 'Turqoise', brand: 'Seccio', price: 485, size: 6, in_stock: 3, id: 1 },
+        { color: 'PowderBlue', brand: 'Seiko', price: 320, size: 7, in_stock: 6, id: 2 },
+        { color: 'Beige', brand: 'Pealy', price: 480, size: 8, in_stock: 12, id: 3 },
+        { color: 'Yellow', brand: 'Spaghetti', price: 220, size: 9, in_stock: 9, id: 4 },
+        { color: 'Brown', brand: 'Chank', price: 150, size: 10, in_stock: 17, id: 5 }
+    ];
+}
